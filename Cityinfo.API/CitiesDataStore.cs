cc27559d30436a2bd6de73bdefa6f3dc8a17801c
Datynford <datynford@gmail.com>
2022-09-29 12:14:31 +0300

    Uploading

---
+++ Cityinfo.API/CitiesDataStore.cs
@@ -0,0 +1,70 @@
+﻿using Cityinfo.API.Models;
+
+namespace Cityinfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park.",
+                    PointsofInterest = new List<PointofInterestDto>()
+                        {
+                        new PointofInterestDto() {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the US."},
+                        new PointofInterestDto() {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown." }
+                       }
+                },
+        
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never really finished.",
+                     PointsofInterest = new List<PointofInterestDto>()
+                        {
+                        new PointofInterestDto() {
+                            Id = 3,
+                            Name = "Cathedral of Our Lady",
+                            Description = "A Gothic style cathedral."},
+                        new PointofInterestDto() {
+                            Id = 4,
+                            Name = "Antwerp Central Station",
+                            Description = "The the finest example of railway architecture in Belgium." }
+                       }
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower.",
+                     PointsofInterest = new List<PointofInterestDto>()
+                        {
+                        new PointofInterestDto() {
+                            Id = 5,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the Champ de Mars."},
+                        new PointofInterestDto() {
+                            Id = 6,
+                            Name = "The Louvre",
+                            Description = "The world's largest museum." }
+                       }
+                }
+            };
+        }
+    }
+}
---
+++ Cityinfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cityinfo.API.Models;
+
+namespace Cityinfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+
+    {
+       [HttpGet]
+       public ActionResult<IEnumerable<PointofInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsofInterest);
+        }
+        [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
+        public ActionResult<PointofInterestDto> GetPointOfInterst(
+            int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterest = city.PointsofInterest
+                .FirstOrDefault(c => c.Id == pointOfInterestId);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+        }
+        [HttpPost]
+        public ActionResult<PointofInterestDto> CreatePointOfIntesert(
+            int cityId,
+            PointOfInterestForCreationDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsofInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointofInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsofInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest",
+                new
+                {
+                    cityId = cityId,
+                    pointOfInterestId = finalPointOfInterest.Id
+                },
+                finalPointOfInterest);
+        }
+    }
+}
